c0b98c13ad25c10199b0de15dc071ec7cc7ecf6a
Johnny_Lemus <artificialmath@gmail.com>
2026-03-09 16:09:30 -0700

    this is the edits to the automatic waffler

---
+++ MYLIBRARY/MyLibrary.cpp
@@ -0,0 +1,123 @@
+// Contains implementations for the hardware abstraction and calibration
+// functions declared in MyLibrary.h.
+
+#include "MyLibrary.h"
+
+#include <HX711_ADC.h>
+#include <EEPROM.h>
+#include <Stepper.h>
+
+// Extern declarations for globals defined in MasterCode_AutoWaffle
+extern const int solenoidPin;
+extern Stepper myStepper;
+extern const int updownsteps;
+extern bool flag;
+
+extern HX711_ADC LoadCell;
+extern const int calVal_eepromAdress;
+
+void openValve() {
+    digitalWrite(solenoidPin, HIGH);
+}
+
+void closeValve() {
+    digitalWrite(solenoidPin, LOW);
+}
+
+void openWaffleIron() {
+    myStepper.step(updownsteps);
+    delay(100);
+    flag = 1;
+}
+
+void closeWaffleIron() {
+    myStepper.step(-updownsteps);
+    flag = 0;
+}
+
+void SystemShutdown() {
+    closeValve();
+    closeWaffleIron();
+    Serial.println("System Shutting Down");
+}
+
+bool waffleIronisOpen() {
+    if (flag == 1) {
+        return true;
+    }
+    return false;
+}
+
+float readForce() {
+    static float filteredForce = 0.0f;
+    const float alpha = 0.15f; // smoothing factor
+
+    float reading = LoadCell.getData();
+
+    if (isnan(reading)) {
+        return filteredForce;
+    }
+
+    filteredForce = alpha * reading + (1.0f - alpha) * filteredForce;
+
+    return filteredForce;
+}
+
+// CALIBRATION METHODS FOR HX711
+void calibrate() {
+    Serial.println("*** Calibration Start ***");
+    Serial.println("Send 't' to tare.");
+
+    while (true) {
+        LoadCell.update();
+        if (Serial.available()) {
+            if (Serial.read() == 't') {
+                LoadCell.tareNoDelay();
+            }
+        }
+        if (LoadCell.getTareStatus()) {
+            break;
+        }
+    }
+
+    Serial.println("Place known mass and enter weight:");
+
+    float known_mass = 0;
+    while (known_mass == 0) {
+        LoadCell.update();
+        if (Serial.available()) {
+            known_mass = Serial.parseFloat();
+        }
+    }
+
+    LoadCell.refreshDataSet();
+    float newCalFactor = LoadCell.getNewCalibration(known_mass);
+
+    Serial.print("New Calibration Value: ");
+    Serial.println(newCalFactor);
+
+    EEPROM.put(calVal_eepromAdress, newCalFactor);
+    Serial.println("Calibration saved.");
+
+    LoadCell.setCalFactor(newCalFactor);
+}
+
+void changeSavedCalFactor() {
+    float oldVal = LoadCell.getCalFactor();
+    Serial.print("Current Cal Factor: ");
+    Serial.println(oldVal);
+
+    Serial.println("Enter new value:");
+
+    float newVal = 0;
+    while (newVal == 0) {
+        if (Serial.available()) {
+            newVal = Serial.parseFloat();
+        }
+    }
+
+    LoadCell.setCalFactor(newVal);
+    EEPROM.put(calVal_eepromAdress, newVal);
+
+    Serial.println("New Cal Factor Saved.");
+}
