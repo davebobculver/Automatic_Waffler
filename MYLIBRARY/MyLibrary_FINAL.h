1716b928c6ad82ec274fd2342a6626dd9626d960
JohnnyDL <64394384+JohnnyDL@users.noreply.github.com>
2026-03-10 19:23:06 -0700

    Create MyLibrary_FINAL.h with function declarations
    Added function declarations and shared interfaces for hardware control and calibration.

---
+++ MYLIBRARY/MyLibrary_FINAL.h
@@ -0,0 +1,23 @@
+// Function declarations and shared interfaces for the AutoWaffle project.
+
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
+// Hardware control helpers
+void openValve();
+void closeValve();
+
+void openWaffleIron();
+void closeWaffleIron();
+
+void SystemShutdown();
+
+bool waffleIronisOpen();
+
+float readForce();
+
+// Calibration helpers for HX711
+void calibrate();
+void changeSavedCalFactor();
+
+#endif // MYLIBRARY_H
